39ed0fdc495c6f7416204c7b6fef86d6968fc883
Alex Ryassky <nosmoke.alex@gmail.com>
2021-12-07 00:08:42 +0300

    Fixes

--- web/index.php
+++ web/index.php
@@ -27,57 +27,55 @@ $app->get('/:url(/:width)', function ($url, $width) use ($app) {
     }
     $filters = [
         'scale'         => 'scale',
-        'CUBIC'         => Imagick::FILTER_CUBIC,
-        'ROBIDOUX'      => Imagick::FILTER_ROBIDOUX,
-        'ROBIDOUXSHARP' => Imagick::FILTER_ROBIDOUXSHARP,
         'LANCZOS'       => Imagick::FILTER_LANCZOS,
         'LANCZOSSHARP'  => Imagick::FILTER_LANCZOSSHARP,
+        'ROBIDOUX'      => Imagick::FILTER_ROBIDOUX,
+        'ROBIDOUXSHARP' => Imagick::FILTER_ROBIDOUXSHARP,
         'LANCZOS2'      => Imagick::FILTER_LANCZOS2,
         'LANCZOS2SHARP' => Imagick::FILTER_LANCZOS2SHARP,
 	/*'BOX'           => Imagick::FILTER_BOX,*/
 	/* 'TRIANGLE'      => Imagick::FILTER_TRIANGLE,*/
         'HERMITE'       => Imagick::FILTER_HERMITE,
-        'HANNING'       => Imagick::FILTER_HANNING,
-        'HAMMING'       => Imagick::FILTER_HAMMING,
+    /*    'HANNING'       => Imagick::FILTER_HANNING,
+        'HAMMING'       => Imagick::FILTER_HAMMING,*/
         'BLACKMAN'      => Imagick::FILTER_BLACKMAN,
         'GAUSSIAN'      => Imagick::FILTER_GAUSSIAN,
         'CATROM'        => Imagick::FILTER_CATROM,
         'MITCHELL'      => Imagick::FILTER_MITCHELL,
         'BESSEL'        => Imagick::FILTER_BESSEL,
-        'KAISER'        => Imagick::FILTER_KAISER,
+/*        'KAISER'        => Imagick::FILTER_KAISER,
         'PARZEN'        => Imagick::FILTER_PARZEN,
-        'WELSH'         => Imagick::FILTER_WELSH,
-	'SINC'          => Imagick::FILTER_SINC,
-    ];
+        'WELSH'         => Imagick::FILTER_WELSH,*/
+        'SINC'          => Imagick::FILTER_SINC,
+    ];    
 
-    $n = 20;    
-    
     echo 'Original: <div style="clear:both: margin-bottom:3rem; width: '.$width.'; height:'.$width.'px;"><img style="width:'.$width.'px; height:'.$width.'px" src="/pics/'.$url.'.jpg"/></div>';
     echo '<div style="clear:both;"> <form action="">'
-    . '     <input type="submit" name="sharpen" value="Резкость"/><br/>
-                Гаусс-радиус<input type="text" name="gauss_radius" value="'.$gaussRadius.'" /><br/>
-                Сигма<input type="text" name="sigma" value="'.$sigma.'" /><br/>
-                Порог<input type="text" name="threshold" value="'.$threshold.'" /><br/>'
-    . '     <input type="submit" name="normal" value="Без резкости"/><br/>';
+    . '     <button type="submit" name="sharpen" value="1" style="margin-right:10px">Резкость</button>
+            <button type="submit" name="normal" value="1">Без резкости</button>
+            <input type="checkbox" name="srgb" value="1" '.(isset($_GET['srgb']) ? 'checked' : '').'>sRGB<br/>
+        <br/>
+    G<input type="text" name="gauss_radius" value="'.$gaussRadius.'" style="width:40px"/>
+    S<input type="text" name="sigma" value="'.$sigma.'" style="width:40px"/>
+    T<input type="text" name="threshold" value="'.$threshold.'" style="width:40px"/><br/>';
     echo '</form></div>';    
     echo '<div style="overflow-x:scroll;overflow-y:hidden;width:'.$width.'px">';
     echo '<div style="display:block; white-space:nowrap;">';
     foreach ($filters as $name => $filter) {
-        $t1 = microtime(true);
-        for ($i = 0; $i<$n; $i++) {
-            $im = clone $imagick;
-            switch ($filter) {
-               case 'scale': $im->scaleImage($width, 0); break;
-               default: 
-                $im->resizeImage($width, 0, $filter, 1); 
-            }
-            if ($sharp) {
-                $im->unsharpMaskImage($gaussRadius, $sigma, 1, $threshold);
-            }
+        $im = clone $imagick;
+        if (isset($_GET['srgb'])) {
+             $image->setImageColorspace(\Imagick::COLORSPACE_SRGB);
         }
-        $t2 = microtime(true);
-        $spent = round(($t2 - $t1)/$n*1000, 1);
-        echo '<div style="display: inline-block; margin:0 5px 0 5px">'.$name.' ('.$spent.')<br/><img src="data:image/' . $imagick->getImageFormat() . ';base64,' . base64_encode($im->getImageBlob()) . '"/></div>';
+        switch ($filter) {
+           case 'scale': $im->scaleImage($width, 0); break;
+           default: 
+           $im->resizeImage($width, 0, $filter, 1); 
+        }
+        if ($sharp) {
+            $im->unsharpMaskImage($gaussRadius, $sigma, 1, $threshold);
+        }
+        echo '<div style="display: inline-block; margin:0 5px 0 5px">'
+            .$name.' <br/><img src="data:image/' . $imagick->getImageFormat() . ';base64,' . base64_encode($im->getImageBlob()) . '"/></div>';
         $im->removeImage();
     }
     echo '</div>';
